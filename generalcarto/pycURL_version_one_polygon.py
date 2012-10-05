36c2cb4de0b5e088edc3422ddbb4bf5fe5928177
TUD-IfC <klammer@klammer-ubuntu.(none)>
2012-10-05 12:52:52 +0200

    cleaning up

--- generalcarto/pycURL_version_one_polygon.py
+++ generalcarto/pycURL_version_one_polygon.py
@@ -127,7 +127,12 @@ def main(filenameI, locationI, nameI, serverI):
                     #print found
                 else:
                     func.writeToLog( "Any problem occured while using pycURL!!!")
-                    wobj = open('/home/klammer/log-files/GeneralCarto-pycURL-ERRORresponse.xml')
+                    ome = os.getenv("HOME")
+        self.generalHome = home + '/GeneralCarto/'
+        if not os.path.isdir(self.generalHome):
+            os.mkdir(self.generalHome)
+        self.logs = self.generalHome + 'log-files/'
+                    wobj = open(os.getenv("HOME")+ 'log-files/GeneralCarto-pycURL-ERRORresponse.xml')
                     wobj.write( response )
                     wobj.close()
                     
